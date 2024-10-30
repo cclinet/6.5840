ab728d7dc646d2abb3e8d52cee894bce2758f830
cclin <cclinet@outlook.com>
2024-10-30 15:04:30 +0800

    update

--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,28 +14,39 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	data map[string]string
 }
-
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	reply.Value = kv.data[args.Key]
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	kv.data[args.Key] = args.Value
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	oldValue := kv.data[args.Key]
+	kv.data[args.Key] = oldValue + args.Value
+	reply.Value = oldValue
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
+	kv.data = make(map[string]string)
 
 	// You may need initialization code here.
 
